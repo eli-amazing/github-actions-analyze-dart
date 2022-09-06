9636a3ec5eed39efa9a39b26169b45dde62cd15b
Eli <eli@amazing.co.il>
2022-09-06 17:22:29 +0300

    Switching to JSON and adding info as notice messages

--- src/index.js
+++ src/index.js
@@ -1,122 +1,131 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const fs = require('fs');
 const path = require('path');
 
 async function run() {
-  try {
-    const workingDirectory = path.resolve(process.env.GITHUB_WORKSPACE, core.getInput('working-directory'))
+    try {
+        const workingDirectory = path.resolve(process.env.GITHUB_WORKSPACE, core.getInput('working-directory'))
 
-    const [analyzeErrorCount, analyzeWarningCount] = await analyze(workingDirectory);
-    const formatWarningCount = await format(workingDirectory);
+        const [analyzeErrorCount, analyzeWarningCount, analyzeInfoCount] = await analyze(workingDirectory);
+        const formatWarningCount = await format(workingDirectory);
 
-    const issueCount = analyzeErrorCount + analyzeWarningCount + formatWarningCount;
-    const failOnWarnings = core.getInput('fail-on-warnings') === 'true';
-    const message = `${issueCount} issue${issueCount === 1 ? '' : 's'} found.`;
+        const issueCount = analyzeErrorCount + analyzeWarningCount + formatWarningCount + analyzeInfoCount;
+        const failOnWarnings = core.getInput('fail-on-warnings') === 'true';
+        const message = `${issueCount} issue${issueCount === 1 ? '' : 's'} found.`;
 
-    if (analyzeErrorCount > 0 || (failOnWarnings && issueCount > 0)) {
-      core.setFailed(message);
-    } else {
-      console.log(message);
+        if (analyzeErrorCount > 0 || (failOnWarnings && issueCount > 0)) {
+            core.setFailed(message);
+        } else {
+            console.log(message);
+        }
+    } catch (error) {
+        core.setFailed(error.message);
     }
-  } catch (error) {
-    core.setFailed(error.message);
-  }
 }
 
 async function analyze(workingDirectory) {
-  let output = '';
+    let output = '';
 
-  const options = { cwd: workingDirectory };
-  options.listeners = {
-    stdout: (data) => {
-      output += data.toString();
-    },
-    stderr: (data) => {
-      output += data.toString();
-    }
-  };
+    const options = {cwd: workingDirectory, silent: true, ignoreReturnCode: true};
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString();
+        },
+        stderr: (data) => {
+            output += data.toString();
+        }
+    };
 
-  const analysisOptionsFile = core.getInput('analysis-options-file');
-  const args = ['--format', 'machine'];
+    const args = ['--format', 'json'];
 
-  if (fs.existsSync(path.resolve(workingDirectory, analysisOptionsFile))) {
-    args.push('--options');
-    args.push(analysisOptionsFile);
-  }
+    args.push('.');
 
-  args.push('.');
+    await exec.exec('dart analyze', args, options);
 
-  await exec.exec('dart analyze', args, options);
+    let errorCount = 0;
+    let warningCount = 0;
+    let noticeCount = 0;
 
-  let errorCount = 0;
-  let warningCount = 0;
-  const lines = output.trim().split(/\r?\n/);
-  const dataDelimiter = '|';
+    const cleanedOutput = output.trim().split(/\n/);
+    const json = cleanedOutput.filter(s => s.startsWith('{')).map(s => s.trim())[0];
 
-  for (const line of lines) {
-    if (!line.includes(dataDelimiter)) {
-      continue;
+    if (!json) {
+        return [errorCount, warningCount, noticeCount];
     }
 
-    const lineData = line.split(dataDelimiter);
-    const lint = lineData[2];
-    const lintLowerCase = lint.toLowerCase();
-    const file = lineData[3].replace(workingDirectory, '');
-    const url = lint === lintLowerCase
-      ? `https://dart-lang.github.io/linter/lints/${lint}.html`
-      : `https://dart.dev/tools/diagnostic-messages#${lintLowerCase}`
-    const message = `file=${file},line=${lineData[4]},col=${lineData[5]}::${lineData[7]} For more details, see ${url}`;
+    const result = JSON.parse(json);
 
-    if (lineData[0] === 'ERROR') {
-      console.log(`::error ${message}`);
-      errorCount++;
-    } else {
-      console.log(`::warning ${message}`);
-      warningCount++;
+    if (!Array.isArray(result.diagnostics)) {
+        return [errorCount, warningCount, noticeCount];
     }
-  }
 
-  return [errorCount, warningCount];
+    for (const diagnostic of result.diagnostics) {
+        const lint = diagnostic.code;
+        const lintLowerCase = lint.toLowerCase();
+        const file = diagnostic.location.file.replace(workingDirectory, '');
+        const line = diagnostic.location.range.start.line;
+        const column = diagnostic.location.range.start.column;
+        const problemMessage = diagnostic.problemMessage;
+        let url = diagnostic.documentation;
+        if (!url) {
+            url = lint === lintLowerCase
+                ? `https://dart-lang.github.io/linter/lints/${lint}.html`
+                : `https://dart.dev/tools/diagnostic-messages#${lintLowerCase}`;
+        }
+        const message = `file=${file},line=${line},col=${column}::${problemMessage} For more details, see ${url}`;
+
+        if (diagnostic.severity === 'ERROR') {
+            console.log(`::error ${message}`);
+            errorCount++;
+        } else if (diagnostic.severity === 'WARNING') {
+            console.log(`::warning ${message}`);
+            warningCount++;
+        } else {
+            console.log(`::notice ${message}`);
+            noticeCount++;
+        }
+    }
+
+    return [errorCount, warningCount, noticeCount];
 }
 
 async function format(workingDirectory) {
-  let output = '';
+    let output = '';
 
-  const options = { cwd: workingDirectory };
-  options.listeners = {
-    stdout: (data) => {
-      output += data.toString();
-    },
-    stderr: (data) => {
-      output += data.toString();
+    const options = {cwd: workingDirectory, silent: true, ignoreReturnCode: true};
+    options.listeners = {
+        stdout: (data) => {
+            output += data.toString();
+        },
+        stderr: (data) => {
+            output += data.toString();
+        }
+    };
+
+    const args = ['format', '--output=none'];
+    const lineLength = core.getInput('line-length');
+
+    if (lineLength) {
+        args.push('--line-length');
+        args.push(lineLength);
     }
-  };
 
-  const args = ['format', '--output=none'];
-  const lineLength = core.getInput('line-length');
+    args.push('.');
 
-  if (lineLength) {
-    args.push('--line-length');
-    args.push(lineLength);
-  }
+    await exec.exec('dart', args, options);
 
-  args.push('.');
+    let warningCount = 0;
+    const lines = output.trim().split(/\r?\n/);
 
-  await exec.exec('dart', args, options);
-  
-  let warningCount = 0;
-  const lines = output.trim().split(/\r?\n/);
+    for (const line of lines) {
+        if (!line.endsWith('.dart')) continue;
+        const file = line.substring(8); // Remove the "Changed " prefix
 
-  for (const line of lines) {
-    if (!line.endsWith('.dart')) continue;
-    const file = line.substring(8); // Remove the "Changed " prefix
+        console.log(`::warning file=${file}::Invalid format. For more details, see https://dart.dev/guides/language/effective-dart/style#formatting`);
+        warningCount++;
+    }
 
-    console.log(`::warning file=${file}::Invalid format. For more details, see https://dart.dev/guides/language/effective-dart/style#formatting`);
-    warningCount++;
-  }
-
-  return warningCount;
+    return warningCount;
 }
 
 run();
